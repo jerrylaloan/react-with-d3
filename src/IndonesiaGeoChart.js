64f4771ad649f99cf0e125da1653ccb60eea7b5d
Jerry Laloan <jerrylaloan@hotmail.com>
2020-04-03 09:56:55 +0700

    Cleanup unused code

--- src/IndonesiaGeoChart.js
+++ src/IndonesiaGeoChart.js
@@ -13,30 +13,6 @@ function IndonesiaGeoChart() {
 
   const [dataIndonesia, setDataIndonesia] = useState(null);
 
-  const _readyFunction = (error, dataIndonesia) => {
-    console.log("_readyFunction -> dataIndonesia", dataIndonesia);
-    setDataIndonesia(dataIndonesia);
-  };
-
-  const _fetchingJsonData = () => {
-    console.log("masuk sini");
-    queue()
-      .defer(
-        d3.json,
-        "http://bl.ocks.org/tvalentius/raw/066b055d4d55de3eb303dc9f3d210d76/bd8310d383d23a08d62e2b22d2daa834f21535d0/indonesia-topojson-city-regency.json"
-      )
-      //   .defer(
-      //     d3.csv,
-      //     "http://bl.ocks.org/tvalentius/raw/066b055d4d55de3eb303dc9f3d210d76/bd8310d383d23a08d62e2b22d2daa834f21535d0/ipm.csv",
-      //     function(d) {
-      //       ipm.set(d.nama_kabkota, Number(d.ipm / 10));
-      //     }
-      //   )
-      .await(_readyFunction);
-  };
-
-  // _fetchingJsonData();
-
   let d3Container = useRef(null);
 
   let projection = d3
@@ -52,15 +28,8 @@ function IndonesiaGeoChart() {
     .domain(d3.range(2, 10))
     .range(d3.schemeYlGnBu[9]);
 
-  // let color = scale.category20();
-
   useEffect(() => {
     (async function fetchinData() {
-      // const result = await fetch(
-      //   "http://bl.ocks.org/tvalentius/raw/066b055d4d55de3eb303dc9f3d210d76/bd8310d383d23a08d62e2b22d2daa834f21535d0/indonesia-topojson-city-regency.json"
-      // );
-      // const jsonData = await result.json();
-      // console.log("fetchinData -> result", jsonData);
       setDataIndonesia(jsonDataIndonesia);
     })();
   }, []);
@@ -88,15 +57,6 @@ function IndonesiaGeoChart() {
         .data(topojson.feature(data, data.objects.IDN_adm_2_kabkota).features)
         .enter()
         .append("path")
-        // .attr("fill", function(d) {
-        //   console.log("IndonesiaGeoChart -> d", d);
-        //   let key = d.properties.NAME_2;
-        //   key = d.properties.VARNAME_2 ? d.properties.VARNAME_2 : key;
-        //   // if (!ipm.get(key)) key = d.properties.NAME_2;
-        //   // if (!ipm.get(key)) console.log(d.properties.VARNAME_2, key);
-
-        //   // return color((d.ipm = ipm.get(key)));
-        // })
         .attr("fill", function(d, i) {
           const rndNumber = Math.random() * (25 - 2) + 2;
           return color(rndNumber.toString());
@@ -117,17 +77,6 @@ function IndonesiaGeoChart() {
       function clicked(d) {
         console.log("clicked -> d", d);
         var x, y, k;
-
-        // if (d) {
-        //   console.log(d.properties);
-        //   document.getElementById("info-location").innerHTML = regionName(d);
-        //   document.getElementById("info-details").innerHTML =
-        //     "Human Development Index : " + getHDI(d);
-        // } else {
-        //   document.getElementById("info-location").innerHTML = "INDONESIA";
-        //   document.getElementById("info-details").innerHTML =
-        //     "Human Development Index (Average) : " + averageIPM;
-        // }
 
         if (d && centered !== d) {
           var centroid = path.centroid(d);
